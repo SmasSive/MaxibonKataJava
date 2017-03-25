b84b56e3e1f07f0a05399ca7cbb789c3f89efaa6
Sergi Castillo Malpesa <sergi.castillo@scmspain.com>
2017-03-25 12:07:24 +0100

    First unit test

--- src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
+++ src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
@@ -2,8 +2,10 @@ package com.karumi.maxibonkata;
 
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnitQuickcheck.class) public class DeveloperProperties {
@@ -15,5 +17,13 @@ import static org.junit.Assert.assertTrue;
     Developer developer = new Developer(ANY_NAME, numberOfMaxibons);
     System.out.println(developer);
     assertTrue(developer.getNumberOfMaxibonsToGrab() >= 0);
+  }
+
+  @Test public void theNumberOfMaxibonsAssignedIsCorrect() throws Exception {
+    assertEquals(1, Karumies.ALBERTO.getNumberOfMaxibonsToGrab());
+    assertEquals(0, Karumies.DAVIDE.getNumberOfMaxibonsToGrab());
+    assertEquals(1, Karumies.JORGE.getNumberOfMaxibonsToGrab());
+    assertEquals(3, Karumies.PEDRO.getNumberOfMaxibonsToGrab());
+    assertEquals(2, Karumies.SERGIO.getNumberOfMaxibonsToGrab());
   }
 }
