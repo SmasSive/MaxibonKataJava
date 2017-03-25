d867419f3fc0b2a5608af34b0ba61980f577ccad
Sergi Castillo Malpesa <sergi.castillo@scmspain.com>
2017-03-25 13:19:59 +0100

    Check message sent for hungry developers and not sent for not so hungry developers

--- src/test/java/com/karumi/maxibonkata/KarumiHQsProperties.java
+++ src/test/java/com/karumi/maxibonkata/KarumiHQsProperties.java
@@ -4,17 +4,26 @@ import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import java.util.List;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 @RunWith(JUnitQuickcheck.class) public class KarumiHQsProperties {
 
-  private static KarumiHQs karumiHQs;
+  @Mock Chat mockChat;
 
-  @BeforeClass public static void setUp() throws Exception {
-    karumiHQs = new KarumiHQs();
+  private KarumiHQs karumiHQs;
+
+  @Before public void setUp() throws Exception {
+    mockChat = mock(Chat.class);
+    karumiHQs = new KarumiHQs(mockChat);
   }
 
   @Property public void theNumberOfMaxibonsIsAlwaysGreaterThanTwo(
@@ -29,5 +38,21 @@ import static org.junit.Assert.assertTrue;
     karumiHQs.openFridge(developers);
     System.out.println("Maxibons left: " + karumiHQs.getMaxibonsLeft());
     assertTrue(karumiHQs.getMaxibonsLeft() > 2);
+  }
+
+  @Property(trials = 5) public void messageIsSentWhenThereAreLessThanTwoMaxibons(
+      @From(HungryDevelopersGenerator.class) Developer developer) {
+    System.out.println(developer);
+    karumiHQs.openFridge(developer);
+    System.out.println("Maxibons left: " + karumiHQs.getMaxibonsLeft());
+    verify(mockChat).sendMessage(anyString());
+  }
+
+  @Property(trials = 5) public void messageIsNotSentWhenThereAreLessThanTwoMaxibons(
+      @From(NotSoHungryDevelopersGenerator.class) Developer developer) {
+    System.out.println(developer);
+    karumiHQs.openFridge(developer);
+    System.out.println("Maxibons left: " + karumiHQs.getMaxibonsLeft());
+    verify(mockChat, never()).sendMessage(anyString());
   }
 }
