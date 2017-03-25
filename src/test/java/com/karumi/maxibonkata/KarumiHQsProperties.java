58fea9933e9e4b853442b776d846ad098ee455f0
Sergi Castillo Malpesa <sergi.castillo@scmspain.com>
2017-03-25 12:16:32 +0100

    Test KarumiHQs have always more than 2 maxibons in the fridge

---
+++ src/test/java/com/karumi/maxibonkata/KarumiHQsProperties.java
@@ -0,0 +1,17 @@
+package com.karumi.maxibonkata;
+
+import com.pholser.junit.quickcheck.From;
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(JUnitQuickcheck.class) public class KarumiHQsProperties {
+
+  @Property public void theNumberOfMaxibonsIsAlwaysGreaterThanTwo(@From(DevelopersGenerator.class) Developer developer) {
+    KarumiHQs karumiHQs = new KarumiHQs();
+    karumiHQs.openFridge(developer);
+    assertTrue(karumiHQs.getMaxibonsLeft() > 2);
+  }
+}
