c94064874197450c24eaf85489a642006669ab2c
Sergi Castillo Malpesa <sergi.castillo@scmspain.com>
2017-03-25 12:36:00 +0100

    Print how many maxibons are left for each iteration

--- src/test/java/com/karumi/maxibonkata/KarumiHQsProperties.java
+++ src/test/java/com/karumi/maxibonkata/KarumiHQsProperties.java
@@ -3,15 +3,22 @@ package com.karumi.maxibonkata;
 import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnitQuickcheck.class) public class KarumiHQsProperties {
 
+  private static KarumiHQs karumiHQs;
+
+  @BeforeClass public static void setUp() throws Exception {
+    karumiHQs = new KarumiHQs();
+  }
+
   @Property public void theNumberOfMaxibonsIsAlwaysGreaterThanTwo(
-      @From(DevelopersGenerator.class) Developer developer) {
-    KarumiHQs karumiHQs = new KarumiHQs();
+      @From(NotSoHungryDevelopersGenerator.class) Developer developer) {
+    System.out.println("Maxibons left: " + karumiHQs.getMaxibonsLeft());
     karumiHQs.openFridge(developer);
     assertTrue(karumiHQs.getMaxibonsLeft() > 2);
   }
